3fd75b969eba5c6720b0e7346b84521ba44403fe
bmarkov <boikom@jqwidgets.com>
2020-10-19 09:55:47 +0300

    Blazor

---
+++ Smart.Blazor.Demos/Program.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Net.Http;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Text;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Smart.Blazor.Demos.Data;
+using Smart.Blazor;
+
+namespace Smart.Blazor.Demos
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            builder.RootComponents.Add<App>("#app");
+
+            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+			builder.Services.AddSmart();
+			builder.Services.AddSingleton<WeatherForecastService>();
+            builder.Services.AddSingleton<RandomDataService>();
+
+            await builder.Build().RunAsync();
+        }
+    }
+}
